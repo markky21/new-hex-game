f8aef497eedf86ad0c8e267cc12592cd77f24233
Marcin Mirecki <mmirecki@pgs-soft.com>
2020-06-20 18:11:37 +0200

    changes

--- src/modules/canvas/Canvas.tsx
+++ src/modules/canvas/Canvas.tsx
@@ -2,6 +2,9 @@ import './three-extend';
 import React from 'react';
 import { Canvas as ThreeCanvas } from 'react-three-fiber';
 import { Scene } from './components/Scene';
+import * as THREE from 'three';
+
+THREE.Object3D.DefaultUp = new THREE.Vector3(0,0,1);
 
 export const Canvas: React.FC = () => {
   return (
