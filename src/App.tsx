313c0558c991b1cabcf3efc356717f2770bffaea
Marcin Mirecki <markky21@gmail.com>
2020-06-20 17:37:30 +0200

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import { Canvas } from './modules/canvas/Canvas';
 
 function App() {
   return (
-    <div style={{ position: 'fixed', width: '100%', height: '100%' }}>
       <Canvas />
-    </div>
   );
 }
 
--- src/modules/canvas/Canvas.tsx
+++ src/modules/canvas/Canvas.tsx
@@ -4,11 +4,12 @@ import { Canvas as ThreeCanvas, useFrame } from 'react-three-fiber';
 export const Canvas: React.FC = () => {
   return (
     <ThreeCanvas style={{ backgroundColor: 'blue' }}>
+      <axesHelper />
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
       <mesh>
         <ringBufferGeometry attach="geometry" args={[10, 10, 6]} />
-        <meshStandardMaterial color={'red'} wireframe={true} attach={'material'} />
+        <meshStandardMaterial color={'red'} attach={'material'} />
       </mesh>
     </ThreeCanvas>
   );
